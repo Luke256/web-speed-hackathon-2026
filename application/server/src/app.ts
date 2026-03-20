04be69beaa04364d24dd54c09009c05cd382e4a5
Luke256 <yuta.k.200628@gmail.com>
2026-03-20 13:31:33 +0900

    Connecttionをcloseしない

--- application/server/src/app.ts
+++ application/server/src/app.ts
@@ -16,7 +16,7 @@ app.use(bodyParser.raw({ limit: "10mb" }));
 app.use((_req, res, next) => {
   res.header({
     "Cache-Control": "max-age=0, no-transform",
-    Connection: "close",
+    // Connection: "close",
   });
   return next();
 });
